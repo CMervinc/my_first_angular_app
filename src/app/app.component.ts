e5a2d19d5c44e921d3b5dfff08df37f360b65228
CMervinc <88871511+CMervinc@users.noreply.github.com>
2022-02-02 22:56:00 +0530

    02_02_2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: ['h1{color:red;}']
+  template: `<h3>Hello world
+  <b>HI</b>
+  </h3><app-child></app-child>`,
+  styles: [`h3
+  {
+    color:blue;
+  }`]
 })
 export class AppComponent {
   title = 'myfirstapp';
